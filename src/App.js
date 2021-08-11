836fe766522233dee885e09b69012879c0c38a5d
AndjelaBabic <andjelasbabic@gmail.com>
2021-08-11 14:43:48 +0200

    Created custom components: FormInput and CustomButton; Used in sign in form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';  
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' render={HomePage}></Route>
         <Route path='/shop' render={ShopPage}></Route>
+        <Route path='/signin'> <SignInAndSignUpPage></SignInAndSignUpPage></Route>
       </Switch>
     </div>
   );
