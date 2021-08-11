eb4954769feb452d9073bbec4fa064866ddd8cb6
AndjelaBabic <andjelasbabic@gmail.com>
2021-08-11 12:31:50 +0200

    Set up shop page with shop items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom'; 
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS Page </h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component';
+import { Switch, Route } from 'react-router-dom';  
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' render={HomePage}></Route>
-        <Route  path='/shop/hats' render={HatsPage}></Route>
+        <Route path='/shop' render={ShopPage}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import Shop from '../../components/shop/shop';
+
+// TODO Check why in shop page I'm not able to use hooks or to have class component 
+
+const ShopPage = () => (
+    <div className='shoppage'>
+        <Shop></Shop>
+    </div>
+   
+)
+
+export default ShopPage; 
